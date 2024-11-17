dd7273c38b3a3fba4b0e76a128a34ebca8400142
Ali200th15 <aliismayilth@gmail.com>
2024-11-17 04:20:18 +0400

    Update

---
+++ ADY.API/ADY.API/Models/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ADY.API.Models
+{
+    public class MyDbContext : DbContext
+    {   
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+          
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
